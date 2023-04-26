6d037e71ef98778bb25406a92db0039ab45d5bba
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-04-26 13:58:15 +0200

    finished

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
 
 window.onscroll = () => {
+    sections.forEach((section) => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 100;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((link) => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
+            })
+            section.classList.add('show-animate');
+        }
+        else {
+            section.classList.remove('show-animate');
+        }
+    })
     let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100  )
+    header.classList.toggle('sticky', window.scrollY > 100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
+
+function abrirTeams() {
+    // URL de enlace profundo de Teams (cámbiala según tus necesidades)
+    var teamsUrl = "https://teams.microsoft.com/l/chat/0/0?users=<jonathan.torresmald@gmail.com>";
+  
+    // Abrir Teams en una nueva ventana o pestaña
+    window.open(teamsUrl, "_blank");
+  }
+
